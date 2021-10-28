92e5593639b46b88f8cb331f19fda86841f983b3
Harinarukula <92969272+Harinarukula@users.noreply.github.com>
2021-10-28 12:30:15 +0530

    Add files via upload

--- hari 3.c
+++ hari 3.c
@@ -1,5 +1,7 @@
 #include<stdio.h>
 void main()
 {
-	printf("Roll\tNo\nName\nBranch\nCollege\n");
+printf("my\rname\ris\rNagabalahari\n");
+printf("\\ this is a back slash symbol and how it is used\n");
+printf("rich dad and poor dad by\"robort");
 }
