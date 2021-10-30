9af30dc73d74931581ec7ae8c9df2be9ef4b7935
Harinarukula <92969272+Harinarukula@users.noreply.github.com>
2021-10-29 23:48:14 -0700

    Add files via upload

---
+++ cvv.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	// Variables
+	float num1,num2,sum;
+	
+	// initialization
+	num1 =9.5;
+	num2 = 8.7;
+	
+	// calculation
+	sum = num1+num2;
+	
+	// output
+	printf("sum of %f and %f is %f",num1,num2,sum);
+	
+}
+
