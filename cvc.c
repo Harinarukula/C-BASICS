81f8af3498738f243bafa19578caf20262fc6fac
Harinarukula <92969272+Harinarukula@users.noreply.github.com>
2021-10-29 23:47:55 -0700

    Add files via upload

---
+++ cvc.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void main()
+{
+	// Variables
+	int num1,num2,sum;
+	
+	// initialization
+	num1 = 125;
+	num2 = 548;
+	
+	// calculation
+	sum = num1+num2;
+	
+	// output
+	printf("%d", sum);
+	
+}
+
