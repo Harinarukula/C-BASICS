ea8005d602bdb6953563243d56a6688c0b665414
Harinarukula <92969272+Harinarukula@users.noreply.github.com>
2021-11-11 02:03:34 -0500

    Add files via upload

---
+++ hari n.c
@@ -0,0 +1,12 @@
+// To find the area and perimeter of a suare 
+#include<stdio.h>
+void main()
+{
+	int s, a, p;
+	printf("enter side");
+	scanf("%d", &s);
+	a = s*s;
+	p = 4*s;
+	printf("area of suare %d");
+	printf("perimeter of suare %d");
+}
